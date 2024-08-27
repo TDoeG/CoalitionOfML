365dd09fc0a9daea55264e6eebd4e88451a69606
TDoeG <89762933+TDoeG@users.noreply.github.com>
2024-08-26 21:53:17 -0700

    Result organized

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,16 +2,12 @@ import os
 from PIL import Image
 
 def convert_images_to_greyscale(source_folder, destination_folder):
+    # Create the destination folder if it doesn't exist
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
+
     # Loop through all the directories and files in the source folder
     for root, dirs, files in os.walk(source_folder):
-        # Create corresponding subdirectories in the destination folder
-        relative_path = os.path.relpath(root, source_folder)
-        dest_subfolder = os.path.join(destination_folder, relative_path)
-        
-        # Create the destination subfolder if it doesn't exist
-        if not os.path.exists(dest_subfolder):
-            os.makedirs(dest_subfolder)
-        
         # Loop through each file in the current directory
         for filename in files:
             # Ensure that filename is a string and then check if it ends with a valid image extension
@@ -25,8 +21,8 @@ def convert_images_to_greyscale(source_folder, destination_folder):
                 # Convert the image to grayscale
                 grayscale_img = img.convert('L')
                 
-                # Construct the full path for the destination image
-                save_path = os.path.join(dest_subfolder, filename)
+                # Construct the full path for the destination image in the main result folder
+                save_path = os.path.join(destination_folder, filename)
                 
                 # Save the grayscale image to the destination folder
                 grayscale_img.save(save_path)
