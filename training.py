c0c8a579676a9bd0bc9bcb28ce4807d830718059
TDoeG <89762933+TDoeG@users.noreply.github.com>
2024-08-27 08:28:32 -0700

    added data splitter

--- training.py
+++ training.py
@@ -28,6 +28,19 @@ Y = torch.tensor(Y).permute(0, 3, 1, 2).float().to(device)
 dataset = TensorDataset(X, Y)
 dataloader = DataLoader(dataset, batch_size=64, shuffle=True, num_workers=8)
 
+# Split dataset into training and testing sets (80% training, 20% testing)
+train_size = int(0.8 * len(X))
+test_size = len(X) - train_size
+train_X, test_X = torch.utils.data.random_split(X, [train_size, test_size])
+train_Y, test_Y = torch.utils.data.random_split(Y, [train_size, test_size])
+
+# Create DataLoader for training and testing sets
+train_dataset = TensorDataset(train_X, train_Y)
+test_dataset = TensorDataset(test_X, test_Y)
+
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=8)
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False, num_workers=8)
+
 # Initialize the model
 model = ColorizationModel().to(device)
 
@@ -66,11 +79,11 @@ with torch.no_grad():
     print(f'Evaluation Loss: {loss / len(dataloader)}')
 
 # Prediction and visualization
-img = Image.open('path_to_image').resize((100, 100))
-img = np.array(img) / 255.0
-X = torch.tensor(color.rgb2gray(img)).unsqueeze(0).unsqueeze(0).float().to(device)
+#img = Image.open('path_to_image').resize((100, 100))
+#img = np.array(img) / 255.0
+#X = torch.tensor(color.rgb2gray(img)).unsqueeze(0).unsqueeze(0).float().to(device)
+#
+#with torch.no_grad():
+#    output = model(X)
 
-with torch.no_grad():
-    output = model(X)
-
-visualize_prediction(img, output)
+#visualize_prediction(img, output)
