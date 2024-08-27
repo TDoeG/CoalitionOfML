acc95ce77d23f96acc77f55f28c23b40f0bbbf56
TDoeG <89762933+TDoeG@users.noreply.github.com>
2024-08-26 17:18:24 -0700

    uhmm what

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # GrayscaleToColorizedML
+`python -m venv somethingml`
+`somethingml\Scripts\activate`
+`git install tensorflow opencv-python numpy matplotlib sklearn-env pandas`
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import cv2
 import matplotlib.pyplot as plt
+import tensorflow as tf
+import keras as k
+import cv2
 from sklearn.model_selection import train_test_split
 from keras.optimizers import Adam
 from keras import backend as K
-from keras.layers import Conv2D,MaxPooling2D,UpSampling2D,Input,BatchNormalization,LeakyReLU
-from keras.layers.merge import concatenate
+from keras.layers import Conv2D,MaxPooling2D,UpSampling2D,Input,BatchNormalization,LeakyReLU,concatenate
 from keras.models import Model
-from keras.preprocessing.image import ImageDataGenerator
-from tensorflow import set_random_seed
-import tensorflow as tf
 
+
+ImagePath = "./assets/images/"
+
+img = cv2.imread(ImagePath+"DSCN9628.JPG")
+img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+img = cv2.resize(img, (224, 224))
+plt.imshow(img)
+img.shape
