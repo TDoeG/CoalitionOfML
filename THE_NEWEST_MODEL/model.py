eaa099419f8be22a228aa50b3f80f43f337c977c
TDoeG <89762933+TDoeG@users.noreply.github.com>
2024-09-04 00:49:39 -0700

    Added second maybe functional model

--- THE_NEWEST_MODEL/model.py
+++ THE_NEWEST_MODEL/model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class Net(nn.Module):
     def __init__(self):
@@ -36,3 +37,47 @@ class Net(nn.Module):
         x = self.dropout(x)
         x = self.fc3(x)
         return x
+    
+
+"""class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # Convolutional layers with BatchNorm
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(128, 256, 3, padding=1)  # Added another conv layer
+        self.bn4 = nn.BatchNorm2d(256)
+        
+        # Pooling layer
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        # Fully connected layers
+        self.fc1 = nn.Linear(256 * 2 * 2, 512)  # Adjusted input size for additional conv layer
+        self.fc2 = nn.Linear(512, 128)
+        self.fc3 = nn.Linear(128, 10)
+        
+        # Dropout
+        self.dropout = nn.Dropout(0.5)  # Increased dropout rate to 50%
+
+    def forward(self, x):
+        # Convolutional layers with ReLU, BatchNorm, and pooling
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+        x = self.pool(F.relu(self.bn4(self.conv4(x))))
+        
+        # Flatten the tensor
+        x = x.view(-1, 256 * 2 * 2)
+        
+        # Fully connected layers with ReLU and dropout
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+        
+        return x"""
